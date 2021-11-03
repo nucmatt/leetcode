d303f9dc82f61d3e467ff22a701df7dc3188deb2
Matthew Wessel <nucmatt@gmail.com>
2021-11-03 11:49:31 -0500

    Second solution for Best Time to Buy and Sell Stock
    This solution discards the window for pure tabulation of prices and
    max profit. This improves memory performance but degrades runtime.
    RunTime: 100ms/66.26% Memory: 48.6MB/69.20%

--- dynamic_programming_easy/buy_sell_stock.js
+++ dynamic_programming_easy/buy_sell_stock.js
@@ -31,8 +31,38 @@ const maxProfit = (prices) => {
     return maxProfit;
 };
 
+const maxProfit2 = (prices) => {
+    // base cases
+    if (prices.length < 2) return 0;
+    // define variables, minBuy, maxSell
+    let minBuy = prices[0], maxSell = prices[1], maxProfit = 0;
+    // loop through prices
+    for (let i = 1; i < prices.length; i++) {
+        // if i < minBuy, minBuy = i, maxSell = i
+        if (prices[i] < minBuy) {
+            minBuy = prices[i];
+            // maxSell must be set to minBuy to ensure the algorithm does NOT look backwards for sell prices BEFORE the buy transaction
+            maxSell = minBuy;
+        }
+        // if i > maxSell, maxSell = i
+        if (prices[i] > maxSell) {
+            maxSell = prices[i];
+        }
+        if (maxProfit < maxSell - minBuy) {
+            maxProfit = maxSell - minBuy;
+        }
+    }
+    // return ternary of profit > 0 return profit, else return 0;
+    return maxProfit
+}
+
 const prices1 = [7,1,5,3,6,4] // 5, p[4] - p[1]
 const prices2 = [7,6,4,3,1] // 0
+const prices3 = [2,4,1]; // 2, p[1] - p[0]
 
 console.log(maxProfit(prices1));
 console.log(maxProfit(prices2));
+console.log(maxProfit(prices3));
+console.log(maxProfit2(prices1));
+console.log(maxProfit2(prices2));
+console.log(maxProfit2(prices3));

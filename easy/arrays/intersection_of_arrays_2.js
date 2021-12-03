11e48783fcce71e92cefbe48de4e9b6fd101e9d8
Matthew Wessel <nucmatt@gmail.com>
2021-12-03 10:03:54 -0600

    Second solution Intersection of 2 Arrays 2
    This solution uses a map to contain each number and the times it appears
    of the first array, then iterates over the second array to find
    intersecting values. Not any faster than my prior solution.
    Runtime: 80ms/67.07% Memory: 40.8MB/36.78%

--- easy/arrays/intersection_of_arrays_2.js
+++ easy/arrays/intersection_of_arrays_2.js
@@ -34,3 +34,35 @@ console.log(intersect(nums7, nums8)
 );
 console.log(intersect(nums5, nums7)
 );
+
+const intersect2 = (nums1, nums2) => {
+    // base cases
+    // define variables
+    // a map to hold the numbers contained in the first array
+    const map = new Map();
+    // an empty array to contain the intersection results
+    const results = [];
+    // logic
+        // iterate over nums1
+        for (const num of nums1) {
+            // if the map already contains the number raise count by 1
+            if (map.has(num)) { 
+                map.set(num, map.get(num) + 1);
+            // else add the number to the map, initialized at 1
+            } else {
+                map.set(num, 1);
+            }
+        }
+        // iterate over nums2
+        for (const num of nums2) {
+            // if map has number and there are still instances left
+            if (map.get(num) > 0) {
+                // push number to results array
+                results.push(num);
+                // decrement instance of number in map
+                map.set(num, map.get(num) - 1);
+            }
+        }
+    // return statement
+    return results;
+}

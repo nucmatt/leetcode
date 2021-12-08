dad16722a21ba1786e04d8cec0124db31583652f
Matthew Wessel <nucmatt@gmail.com>
2021-12-08 12:22:07 -0600

    Second Solution First Unique Character
    This is an optimized solution for my previous solution. This version is
    faster because lastIndexOf requires finding each instance of the
    character to determine which is last, where indexOf only needs to find
    the first match after a starting position.
    Runtime: 80ms/99.66% Memory: 42MB/74.15%

--- easy/strings/first_unique_character.js
+++ easy/strings/first_unique_character.js
@@ -17,6 +17,17 @@ const firstUniqChar = (s) => {
     // if all characters are repeated, return -1
 }
 
+// The dual conditionals make the runtime faster because lastIndex of has to go through ALL instances of the character to find the last, where as just using indexOf and starting after the currect characters index only needs to find the next instance, if any.
+const firstUniqCharOptimized = (s) => {
+    for (i = 0; i < s.length; i++) {
+        let match = s[i];
+        if(s.indexOf(match) === i && s.indexOf(match, i + 1) === -1) {
+            return i
+        } 
+    }
+    return -1;
+};
+
 console.log(firstUniqChar(s1));
 console.log(firstUniqChar(s2));
 console.log(firstUniqChar(s3));

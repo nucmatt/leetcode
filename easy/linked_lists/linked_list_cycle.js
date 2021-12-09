b4ea5ec61e675ad8fbe0960c153fad18cc4adc95
Matthew Wessel <nucmatt@gmail.com>
2021-12-09 10:18:38 -0600

    Second Solution Linked List Cycle
    This solution uses Floyd's algorithm. This works because with a cycle
    the faster pointer will eventually equal the slower pointer.
    Runtime: 80ms/83.53% Memory: 41.5MB/41.06%

--- easy/linked_lists/linked_list_cycle.js
+++ easy/linked_lists/linked_list_cycle.js
@@ -5,23 +5,44 @@
 // Return true if there is a cycle in the linked list. Otherwise, return false.
 
 const hasCycle = (head) => {
-    // base cases
-    if (head === null) return false;
-    // define variables
-    // a set to contain each seen node since each node should only be seen once
-    const set = new Set();
-    let currNode = head;
-    // logic
-    // loop through list while current node next is not null
-    while (currNode.next !== null) {
-        // if the set has the current node, return true
-        if (set.has(currNode)) return true; 
-        // if set does not have the node, add the node to the set
-        set.add(currNode);
-        // move current node up one node
-        currNode = currNode.next;
-    }
-    // return statements
-    // if the loop completes then the end of the list has been found so no cycle
-    return false;
-}
+	// base cases
+	if (head === null) return false;
+	// define variables
+	// a set to contain each seen node since each node should only be seen once
+	const set = new Set();
+	let currNode = head;
+	// logic
+	// loop through list while current node next is not null
+	while (currNode.next !== null) {
+		// if the set has the current node, return true
+		if (set.has(currNode)) return true;
+		// if set does not have the node, add the node to the set
+		set.add(currNode);
+		// move current node up one node
+		currNode = currNode.next;
+	}
+	// return statements
+	// if the loop completes then the end of the list has been found so no cycle
+	return false;
+};
+
+const hasCycleFloyds = (head) => {
+	// base cases
+	if (head === null) return false;
+	// define variables
+	// two pointers
+	let tortoise = head,
+		hare = head;
+	// logic
+	// while hare and hare's next is not null (both are needed to prevent a runtime error for no next property)
+	while (hare !== null && hare.next !== null) {
+		// tortoise moves 1, hare moves 2
+		tortoise = tortoise.next;
+		hare = hare.next.next;
+		// if tortoise === hare, return true
+		if (tortoise === hare) return true;
+	}
+	// return statement
+	// if the loop completes then a null has been reach so no cycle
+	return false;
+};

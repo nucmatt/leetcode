cc989d76fc2aabccab3a635fd1e99817936c213c
Matthew Wessel <nucmatt@gmail.com>
2022-03-23 10:37:35 -0500

    First solution Largest Perimeter Triangle
    This solution sorts the array and uses Triangle Inequality Theorem 1 to
    find the largest possible triangle. A way to improve memory usage may be
    to sort the array highest to lowest and the use a sliding window instead
    of storing each side as an individually defined variable.
    Runtime: 92ms/94.96% Memory: 45.6MB/41.67%

---
+++ easy/math/largest_perimeter_triangle.js
@@ -0,0 +1,54 @@
+// Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+// Example 1:
+
+// Input: nums = [2,1,2]
+// Output: 5
+
+// Example 2:
+
+// Input: nums = [1,2,1]
+// Output: 0
+
+// Constraints:
+
+//     3 <= nums.length <= 10^4
+//     1 <= nums[i] <= 10^6
+
+const largestPerimeter = (nums) => {
+	// base cases
+	// define variables
+	let sideA;
+	let sideB;
+	let sideC;
+	// logic
+	// all triangles require that the sum of the lengths of the two shorter sides must be greater than the length of the third side(Triangle Inequality Theorem 1).
+	// first we should sort the nums array lowest to highest
+	nums.sort((a, b) => a - b);
+	// pop off the two largest values and set them equal to sideA and sideB, in that order
+	sideA = nums.pop();
+	sideB = nums.pop();
+	// while there are still numbers in the nums array
+	while (nums.length > 0) {
+		// pop off a number from the nums array and set it equal to side C
+		sideC = nums.pop();
+		// console.log(sideA, sideB, sideC);
+		// if sideA equals sideB return the sum of the sides. We can do this because the array is sorted so sideC has to be equal to or less than both sides.
+		// if sideB plus SideC >= SideA, return the sum of the sides as the triangle satisfies Triangle Inequality Theorem 1.
+		if (sideB + sideC > sideA) {
+			return sideA + sideB + sideC;
+		}
+		// if we haven't formed a triangle set sideA equal to sideB and then sideB equal to sideC. Now the loop will go again to check the next number as sideC
+        [sideA, sideB] = [sideB, sideC];
+        // console.log(sideA, sideB, sideC);
+	}
+
+	// return statement
+	// if the loop completes then we have not formed a triangle
+    return 0;
+};
+
+console.log(largestPerimeter([2, 1, 2]));
+console.log(largestPerimeter([2, 1, 3, 4]));
+console.log(largestPerimeter([2, 1, 1]));
+console.log(largestPerimeter([2, 1, 1, 3]));
